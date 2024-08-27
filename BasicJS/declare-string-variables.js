d531143d853c1948b22b4276ea5efad09e062a3c
Nastya-99 <nastyalakisovaa@gmail.com>
2024-08-27 14:49:02 +0300

    declare-string-variables.js

---
+++ BasicJS/declare-string-variables.js
@@ -0,0 +1,11 @@
+// ❓ DESCRIPTION:
+//Создайте две новые строковые переменные: myFirstName и myLastName и присвойте им значения вашего имени и фамилии соответственно.
+
+
+// ✅ SOLUTION:
+
+let myFirstName = 'Настя';
+let myLastName = "Краснонос";
+console.log(myFirstName + ' ' + myLastName);
+
+

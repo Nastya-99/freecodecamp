67d7672e959dc031662aec93d9837afdf0f4a8ac
Nastya-99 <nastyalakisovaa@gmail.com>
2024-08-27 23:33:18 +0300

    increment-a-number-with-js.js

---
+++ BasicJS/increment-a-number-with-js.js
@@ -0,0 +1,12 @@
+// ❓ DESCRIPTION:
+
+//Изменение кода для использования оператора ++ в myVar.
+//myVar должен быть равен 88.
+
+
+// ✅ SOLUTION:
+let myVar = 87;
+
+// Only change code below this line
+myVar++;
+console.log(myVar);

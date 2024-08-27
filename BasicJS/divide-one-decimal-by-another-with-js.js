3de6c1a6bebb23c6451be9796c704bfe30e42afe
Nastya-99 <nastyalakisovaa@gmail.com>
2024-08-28 00:08:08 +0300

    divide-one-decimal-by-another-with-js.js

---
+++ BasicJS/divide-one-decimal-by-another-with-js.js
@@ -0,0 +1,8 @@
+// ❓ DESCRIPTION:
+
+//Измените 0.0так, чтобы quotient получилось 2.2.
+//const quotient = 0.0 / 2.0; // Change this line
+
+// ✅ SOLUTION:
+const quotient = 4.4 / 2.0; // Change this line
+console.log(quotient);

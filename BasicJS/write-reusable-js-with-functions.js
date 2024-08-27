63dd283cfe910869df199d8d661fbc137e1c3053
Nastya-99 <nastyalakisovaa@gmail.com>
2024-08-27 18:00:31 +0300

    write-reusable-js-with-functions.js

---
+++ BasicJS/write-reusable-js-with-functions.js
@@ -0,0 +1,10 @@
+// ❓ DESCRIPTION:
+//Создайте функцию reusableFunction, которая выводит строку Hi Worldв консоль разработчика.
+// Вызовите функцию.
+
+// ✅ SOLUTION:
+
+function reusableFunction() {
+    console.log('Hi World');
+}
+reusableFunction();

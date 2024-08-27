f2831b24afc9311713f6466fed61f170cbf967ce
Nastya-99 <nastyalakisovaa@gmail.com>
2024-08-27 16:02:21 +0300

    declare-a-read-only-variable-with-the-const-keyword.js

---
+++ BasicJS/declare-a-read-only-variable-with-the-const-keyword.js
@@ -0,0 +1,16 @@
+// ❓ DESCRIPTION:
+
+//Измените код так, чтобы все переменные были объявлены с помощью let или const. Используйте, let когда вы хотите, чтобы переменная изменилась, и const когда вы хотите, чтобы переменная оставалась постоянной. Также переименуйте переменные, объявленные с помощью , const чтобы соответствовать общепринятым практикам. Не изменяйте строки, назначенные переменным.
+
+//var fCC = "freeCodeCamp"; // Change this line
+// var fact = "is cool!"; // Change this line
+// fact = "is awesome!";
+// console.log(fCC, fact); // Change this line
+
+
+// ✅ SOLUTION:
+
+const FCC = "freeCodeCamp"; // Change this line
+let fact = "is cool!"; // Change this line
+fact = "is awesome!";
+console.log(FCC, fact); // Change this line

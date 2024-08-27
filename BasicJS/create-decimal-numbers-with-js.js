60a8fdd8e516edcf3fce3dd39bf4f6445b519c2b
Nastya-99 <nastyalakisovaa@gmail.com>
2024-08-27 23:47:11 +0300

    create-decimal-numbers-with-js.js

---
+++ BasicJS/create-decimal-numbers-with-js.js
@@ -0,0 +1,15 @@
+// ❓ DESCRIPTION:
+
+//Создайте переменную myDecimal и присвойте ей десятичное значение с дробной частью (например 5.7, ).
+//myDecimal должно быть число.
+// Ожидающий :myDecimal должна иметь десятичную точку
+
+// ✅ SOLUTION:
+
+const ourDecimal = 5.7;
+
+// Only change code below this line
+
+const myDecimal = 5.7;
+
+console.log(myDecimal);

64d14ecfb831af69a4ab45463bcff46bee015d8c
Nastya-99 <nastyalakisovaa@gmail.com>
2024-08-27 23:38:20 +0300

    decrement-a-number-with-js.js

---
+++ BasicJS/decrement-a-number-with-js.js
@@ -0,0 +1,13 @@
+// ❓ DESCRIPTION:
+
+// Измените код, чтобы использовать --оператор на myVar.
+//Ожидающий :myVarдолжно быть равно 10.
+
+// ✅ SOLUTION:
+
+let myVar = 11;
+
+// Only change code below this line
+myVar--;
+console.log(myVar);
+

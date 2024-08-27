73ed0d427b5146716c163723ff609d0ea873fc66
Nastya-99 <nastyalakisovaa@gmail.com>
2024-08-27 19:11:42 +0300

    returna-a-value-from-a-function-with-return.js

---
+++ BasicJS/returna-a-value-from-a-function-with-return.js
@@ -0,0 +1,16 @@
+// ❓ DESCRIPTION:
+
+//Создайте функцию timesFive, которая принимает один аргумент, умножает его на 5 и возвращает новое значение.
+
+// ✅ SOLUTION:
+
+function timesFive(num1) {
+    return num1 * 5;
+}
+const five = timesFive(5);
+const two = timesFive(2);
+const zero = timesFive(0);
+
+console.log(five);
+console.log(two);
+console.log(zero);

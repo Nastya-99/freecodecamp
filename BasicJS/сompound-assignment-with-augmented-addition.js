a7ee860fb5ff62acfd4dc78775839f926cd2a31e
Nastya-99 <nastyalakisovaa@gmail.com>
2024-08-28 23:22:10 +0300

    сompound-assignment-with-augmented-addition.js

---
+++ BasicJS/сompound-assignment-with-augmented-addition.js
@@ -0,0 +1,26 @@
+// ❓ DESCRIPTION:
+
+//Преобразуйте присваивания для a, b и c чтобы использовать +=оператор.
+//let a = 3;
+// let b = 17;
+// let c = 12;
+//
+// // Only change code below this line
+// a = a + 12;
+// b = 9 + b;
+// c = c + 7;
+
+// ✅ SOLUTION:
+
+let a = 3;
+let b = 17;
+let c = 12;
+
+// Only change code below this line
+a += 12;
+b += 9;
+c += 7;
+
+console.log(a);
+console.log(b);
+console.log(c);

1b517f1c4ed367cd097261753dd6aed0b52af636
Nastya-99 <nastyalakisovaa@gmail.com>
2024-08-28 23:56:33 +0300

    escaping-literal-quotes-in-strings.js

---
+++ BasicJS/escaping-literal-quotes-in-strings.js
@@ -0,0 +1,9 @@
+// ❓ DESCRIPTION:
+
+//Используйте обратные косые черты , чтобы присвоить строку переменной myStr, так что если вы выведете ее на консоль, то увидите:
+// I am a "double quoted" string inside "double quotes".
+//const myStr = ""; // Change this line
+// ✅ SOLUTION:
+
+const myStr = "I am a \"double quoted\" string inside \"double quotes\"."; // Change this line
+console.log(myStr);

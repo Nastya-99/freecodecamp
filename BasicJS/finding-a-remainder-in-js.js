66d394e4d8e9b72d2c659214bcd579a5855df4af
Nastya-99 <nastyalakisovaa@gmail.com>
2024-08-28 23:13:47 +0300

    finding-a-remainder-in-js.js

---
+++ BasicJS/finding-a-remainder-in-js.js
@@ -0,0 +1,9 @@
+// ❓ DESCRIPTION:
+
+//Приравняйте remainder к остатку от 11деления, 3используя оператор остатка ( %).
+//const remainder = 0;
+
+// ✅ SOLUTION:
+
+const remainder = 11 % 3;
+console.log(remainder);

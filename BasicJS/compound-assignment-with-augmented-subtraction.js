ab81e8141a08a8cd3aeb74c0fd35b5c9ca95c7ae
Nastya-99 <nastyalakisovaa@gmail.com>
2024-08-28 23:27:42 +0300

    compound-assignment-with-augmented-subtraction.js

---
+++ BasicJS/compound-assignment-with-augmented-subtraction.js
@@ -0,0 +1,18 @@
+// ❓ DESCRIPTION:
+
+//Преобразуйте присваивания для a, bи , cчтобы использовать -=оператор.
+
+// ✅ SOLUTION:
+
+let a = 11;
+let b = 9;
+let c = 3;
+
+// Only change code below this line
+a -= 6;
+b -= 15;
+c -= 1;
+
+console.log(a);
+console.log(b);
+console.log(c);

b5d2e41503befd67bc356439a09381f1053099e2
Nastya-99 <nastyalakisovaa@gmail.com>
2024-08-30 00:32:29 +0300

    concatenating-strings-with-plus-operator.js

---
+++ BasicJS/concatenating-strings-with-plus-operator.js
@@ -0,0 +1,6 @@
+// ❓ DESCRIPTION:
+
+//Постройте myStrиз строк This is the start.и This is the end.используйте +оператор. Обязательно включите пробел между двумя строками.
+// ✅ SOLUTION:
+const myStr = "This is the start." + " "+ "This is the end."; // Change this line
+console.log(myStr);
